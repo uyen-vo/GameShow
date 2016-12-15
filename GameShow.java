8608761fac51063fb3af9516fb1ce0bcb9739138
Uyen Vo <uyenvo@tamu.edu>
2016-12-15 04:35:55 -0600

    Added restart, "true" case and numeric support, and randomized the questions; about to begin on multiplayer

--- GameShow.java
+++ GameShow.java
@@ -11,29 +11,32 @@ import java.util.Scanner;
 
 //add multiplayer
 //add restart
-
+//difficulty/levels? -> faster time
 //static -> inner classes don't have access to private of outer class
+//add random questions
 
 public class GameShow {
 	private static int points;
-	private static Lock lock;
+	private static Lock lock = new ReentrantLock(); //for the lock.unlock() at the finally of PrintQ
 	private static int timeFlag;
 	private static boolean answered; 
 	private static Condition answer;
+	private static boolean intQuit; //interrupted quit
+	private static ArrayList<String> answers = new ArrayList();
 	
 	public GameShow(){
 		//constructor to initialize
 		points = 0;
-		lock = new ReentrantLock();
 		timeFlag = 0;
 		answered = false;
 		answer = lock.newCondition();
+		intQuit = false;
 	}
 
 	public static void main(String[] args) {
 		// Create tasks
 		GameShow g = new GameShow();
-		System.out.println("Welcome to the game. Case matters. Enter \"X\" to quit.");
+		System.out.println("Welcome to the game. Enter \"X\" to quit at any time.");
 		Runnable printQuestion = new PrintQ();
 		//Runnable restart = new Restart();
 		    
@@ -62,16 +65,30 @@ public class GameShow {
 //			
 //		}
 //	}
+	
+	private static boolean isNumeric(String s) {  
+	    return s.matches("[-+]?\\d*\\.?\\d+");  
+	}  
+	
 	public static class PrintQ implements Runnable{
 			private ArrayList<String> questions = new ArrayList();
 			private Scanner sc = new Scanner(System.in);
 			private String userAns;
+			private int newQ;
+			private int numQ;
 
 			public PrintQ(){
 				questions.add("Who was the first president of the U.S.?");
 				questions.add("What is Obama's first name?");
 				questions.add("What is Professor Dutta's first name?");
 				questions.add("What is 2+2? (Number)");
+				answers.add("George Washington");
+				answers.add("Barack");
+				answers.add("Anandi");
+				answers.add("4");
+				userAns = "";
+				newQ = questions.size();
+				numQ = questions.size();
 			}
 			
 			
@@ -79,23 +96,31 @@ public class GameShow {
 			public void run() {
 				lock.lock();
 				try{
-				for(int i = 0; i<questions.size(); i++){
+				for(int i = 0; i<numQ; i++){
+					int pick = (int)(Math.random() * newQ);
+					newQ--;		
 					System.out.println("Current points: " + points);
-					System.out.println(questions.get(i));
+					System.out.println(questions.get(pick));
+					questions.remove(pick); //randomize questions
 					timeFlag = 0;
 					answered = false;
 					Runnable timer = new TimeTen();
 					Thread threadT = new Thread(timer);
 				    threadT.start();
 				    
-					userAns = sc.nextLine();
+					userAns = sc.nextLine(); 
+				    if(!isNumeric(userAns)){ //toLowerCase seems to mess up numeric strings
+				    	String temp = userAns.toLowerCase(); // case support
+				    	userAns = temp;
+				    }
 				    
-					if(userAns.equals("X")){
-						threadT.interrupt();
+					if(userAns.equals("X") || userAns.equals("x")){
+						threadT.interrupt(); //stop the timer
+						intQuit = true;
 						break;						
 					}
-				    
-				    Runnable reader = new Read(i, userAns);
+					
+				    Runnable reader = new Read(pick, userAns);
 				    Thread threadR = new Thread(reader);
 				    threadR.start();
 				    answer.await();
@@ -107,15 +132,24 @@ public class GameShow {
 				    
 				}
 				System.out.println("Game end! Total points: " + points);
-				System.out.println("Would you like to play again? (y/n)");
-				String a = sc.nextLine();
-				if(a.equals("y") || a.equals("Y")){
-					//call the main method
-					String[] args={};
-					GameShow.main(args);
+				
+				if(!intQuit){
+					System.out.println("Would you like to play again? (\"y\" for yes)");
+					String a = sc.nextLine();
+					if(a.equals("y") || a.equals("Y")){
+						//call the main method of the "next" game instance
+						String[] args={};
+						GameShow.main(args);
+						System.out.println("");
+					}
 				}
+				
+				else{
+					sc.close(); //close the scanner
+				}
+				
 
-				sc.close(); //close the current instance of the game
+				//close the current instance of the game
 
 				} catch (Exception e){
 					e.printStackTrace();
@@ -129,16 +163,21 @@ public class GameShow {
 	  
 	public static class Read implements Runnable{
 
-		ArrayList<String> answers = new ArrayList();
+		String trueAnsLOW;
 		String trueAns;
 		String userAns;
 		
 		public Read(int ansIndex, String userAns){
-			answers.add("George Washington");
-			answers.add("Barack");
-			answers.add("Anandi");
-			answers.add("4");
 			trueAns = answers.get(ansIndex);
+			if(!isNumeric(trueAns)){ //toLowerCase seems to mess up numeric strings
+		    	String temp = trueAns.toLowerCase(); // case support
+		    	trueAnsLOW = temp;
+		    }
+			else{
+				//if it is numeric then it is the same, no lower case format
+				trueAnsLOW = trueAns;
+			}
+			answers.remove(ansIndex);
 			this.userAns = userAns;
 		}
 		
@@ -147,7 +186,7 @@ public class GameShow {
 			//read in answer
 			try{
 				if(timeFlag == 0){
-					if(userAns.compareTo(trueAns)==0){
+					if(userAns.compareTo(trueAnsLOW)==0){
 						points++;
 						System.out.println("Correct!\n");
 					}
